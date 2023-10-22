4135636aa1b6987ca090f32af81792dd4658d65d
NoeTotadze <noe.totadzee@gmail.com>
2023-10-22 14:45:00 +0400

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,17 @@
+name = "noe" #ეს არის STR(STRING) ტიპის ცვლადი
+surname = "totadze"
+age = 25 #ეს არის int (integer) მთელი რიცხვი
+height = 178 # ეს არის float ტიპის ცვლადი
+#boolean (bool) ტიპის ცვლადი
+knows_programming = True
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#name არის ცვლადი
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+is_ugly = False #snakecase (უბრალოდ წერის სტანდარტული სტილი)
+isugly = False #ჯავასკრიპტული (camelcase)
+
+surname = "totadze"
+print("me var " + name + " " + surname + " " + str(age) +" wlis " + str(height) + " santimetri simaglis ")
+
+
+
